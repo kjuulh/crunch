d4d537439299a8f6bec1f23fbd2c081b68a51e91
kjuulh <contact@kjuulh.io>
2023-09-18 21:25:45 +0200

    feat: with envelope tests
    Signed-off-by: kjuulh <contact@kjuulh.io>

---
+++ crates/test-bin/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    #[no_mangle]
+    fn envelope_capnp_benchmark(content: &[u8]) -> () {
+        let out = crunch_envelope::wrap("some-domain", "some-entity", content);
+
+        let out = crunch_envelope::unwrap(&out).expect("to be able to unwrap capnp message");
+
+        println!("{:?}", out.1);
+    }
+
+    #[no_mangle]
+    fn envelope_json_benchmark(content: &[u8]) -> () {
+        let out = crunch_envelope::json::wrap("some-domain", "some-entity", content);
+
+        let out = crunch_envelope::json::unwrap(&out).expect("to be able to unwrap capnp message");
+
+        println!("{:?}", out.1);
+    }
+
+    let large_content: [u8; 1000000] = [0; 1000000];
+
+    _ = envelope_capnp_benchmark(&large_content);
+
+    _ = envelope_json_benchmark(&large_content);
+
+    println!("done")
+}
