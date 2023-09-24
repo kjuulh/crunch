4e70adfb38e634c9a51daa0d3bd54b32548f9528
kjuulh <contact@kjuulh.io>
2023-09-24 10:48:41 +0200

    feat: add basic example
    Signed-off-by: kjuulh <contact@kjuulh.io>

---
+++ examples/basic-setup/src/main.rs
@@ -0,0 +1,40 @@
+use crunch::traits::{Deserializer, Event, EventInfo, Serializer};
+
+struct MyEvent {}
+
+impl Serializer for MyEvent {
+    fn serialize(&self) -> Result<Vec<u8>, crunch::errors::SerializeError> {
+        todo!()
+    }
+}
+impl Deserializer for MyEvent {
+    fn deserialize(_raw: Vec<u8>) -> Result<Self, crunch::errors::DeserializeError>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+impl Event for MyEvent {
+    fn event_info() -> crunch::traits::EventInfo {
+        EventInfo {
+            domain: "my-domain",
+            entity_type: "my-entity-type",
+            event_name: "my-event-name",
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let crunch = crunch::builder::Builder::default().build()?;
+
+    crunch
+        .subscribe(|_item: MyEvent| async move { Ok(()) })
+        .await?;
+
+    crunch.publish(MyEvent {}).await?;
+
+    Ok(())
+}
